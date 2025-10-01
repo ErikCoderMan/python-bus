b7bdf990d9a403337a541bbf03148724f605d761
Erik A <196097247+ErikCoderMan@users.noreply.github.com>
2025-10-01 18:34:00 +0200

    RemovePassenger class can now successfully remove a passenger

--- remove_passenger.py
+++ remove_passenger.py
@@ -1,7 +1,24 @@
 class RemovePassenger:
     def __init__(self, passengers):
-        self.passengers = passengers
+        self.passengers = passengers # this list will be changed "in place"
         
     def Start(self):
-        print("inside RemovePassenger Start method\n")
+        # show list with index
+        for i, passenger in enumerate(self.passengers):
+            print(f"id: {i}, {passenger}")
         
+        # ask for what index to remove
+        try:
+            answer = int(input("Enter id to remove: "))
+            if 0 <= answer < len(self.passengers):
+                removed = self.passengers.pop(answer)
+                print(f"Removed: {removed}")
+                
+            else:
+                print("Invalid id")
+                
+        except ValueError:
+            print("Please enter a number")
+        
+        # newline
+        print("")
