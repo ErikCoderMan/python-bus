d2bc84b3521770d243b9650d5601c2666578d326
davidfjellstrom <56550285+davidfjellstrom@users.noreply.github.com>
2025-10-04 14:35:16 +0200

    Updated comments in main.py

--- average_age.py
+++ average_age.py
@@ -1,7 +1,22 @@
 class AverageAge:
+    #Kontruktorn (_init_) körs när vi skapar objektet i main.py
+    # Den tar in listan "passengers" (alla passangerare från JSON-filen)
+    
     def __init__(self, passengers):
-        self.passengers = passengers
-        
+        self.passengers = passengers # Sparar listan på objektet så vi kan använda den senare
+
     def Start(self):
-        print("inside AverageAge Start method\n")
-        
+        if not self.passengers:
+            print("No passengers are on the bus.") # Skriver ut meddelande om att det inte finns några passangerare på bussen och avslutar.
+            return
+
+        # Skapar tom lista för alla åldrar    
+        ages = []
+        for passenger in self.passengers:
+            if "age" in passenger:
+                ages.append(passenger["age"])
+
+        # Om ingen har ålder lagrad, skriver ut meddelande och avslutar.
+        if not ages:
+            print("No ages available for passengers.")
+            return
