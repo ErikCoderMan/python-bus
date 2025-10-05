055bffde4f5abfd772d5899adba5864e2c4d4e4a
Asad8687 <asadm8687@gmail.com>
2025-10-05 23:20:03 +0200

    Uppdated the code and added intern library

--- add_passenger.py
+++ add_passenger.py
@@ -1,10 +1,19 @@
+import datetime
+import json
+
+
 class passenger:
-    def __init__(self, age, gender):
+    def __init__(self, name, age, gender, time):
+        self.name = name
         self.age = age
         self.gender = gender
+        self.time = time
     def __str__(self):
-        return f"passenger(age: {self.age},gender: {self.gender})"
+        return f"passenger(name: {self.name},age: {self.age}, gender: {self.gender}, added at {self.time})"
 class bus:
-    def __init__ (self, capacity = 10)
+    def __init__ (self, capacity = 10):
+        self.capacity = capacity
+        self.passengers = []
+        
 
         
