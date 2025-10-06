f081e9ab029960704c56a33d7ac51bc7209e592a
Gabriella Cross <gabriella.cross@hotmail.com>
2025-10-06 13:34:59 +0200

    test commit 2

--- show_passengers.py
+++ show_passengers.py
@@ -8,9 +8,15 @@ class ShowPassengers:
             return
         print("Passengers:")
         for passenger in self.passengers:
-            print("-", passenger)
+            print(passenger)
 
 
 if __name__ == "__main__":
-            ShowPassengers(["Alice","Bob","Charlie"]).Start()
+            test_passengers = []
+            test_passengers.append({"name": "Alice", "age": 20, "gender": "Female", "time": "10:00:00"})
+            test_passengers.append({"name": "Bob", "age": 25, "gender": "Male", "time": "11:00:00"})
+
+            # create object of ShowPassengers class and call Start method
+            show_passengers = ShowPassengers(test_passengers)
+            show_passengers.Start()
         
