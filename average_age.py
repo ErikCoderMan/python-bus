5830152f1ecaceed49be5712d2fc77d3ade1b618
davidfjellstrom <56550285+davidfjellstrom@users.noreply.github.com>
2025-10-06 14:42:03 +0200

    Add average_age feature

--- average_age.py
+++ average_age.py
@@ -4,19 +4,26 @@ class AverageAge:
     
     def __init__(self, passengers):
         self.passengers = passengers # Sparar listan på objektet så vi kan använda den senare
-
+    
+    # Metoden start körs när användaren väljer menyval 4 i main-filen
     def Start(self):
+        # Om det inte finns några passangerare alls, skriver ut sträng och avslutar.
         if not self.passengers:
-            print("No passengers are on the bus.") # Skriver ut meddelande om att det inte finns några passangerare på bussen och avslutar.
+            print("No passengers are on the bus.")
             return
 
         # Skapar tom lista för alla åldrar    
         ages = []
-        for passenger in self.passengers:
-            if "age" in passenger:
-                ages.append(passenger["age"])
+        for passenger in self.passengers: # Går igenom varje passangerare
+            if "age" in passenger: # Kollar att passangerare har en ålder
+                ages.append(passenger["age"]) # Lägger till åldern i listan ages
 
         # Om ingen har ålder lagrad, skriver ut meddelande och avslutar.
         if not ages:
             print("No ages available for passengers.")
             return
+        
+        # Räknar ut medelåldern = summan av alla åldrar/antalet passangerare
+        average = sum(ages)/len(ages)
+        # Skriver ut genomsnittsåldern med en decimal
+        print(f"Average age of passengers is: {average:1f} years")
