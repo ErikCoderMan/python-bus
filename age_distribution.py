5e94f19faa2388d1e5207e44df6da884482cddb0
Erik A <196097247+ErikCoderMan@users.noreply.github.com>
2025-10-08 18:11:37 +0200

    Improved comments

--- age_distribution.py
+++ age_distribution.py
@@ -18,12 +18,14 @@ class AgeDistribution:
         max_age = max(ages)
         bins = range(0, (max_age // 10 + 2) * 10, 10)
         plt.hist(ages, bins=bins, edgecolor='black', alpha=0.7)
+        
+        # Make Y-axis display only whole numbers
+        # since fractional passengers don't make sense
         ax = plt.gca()
         ax.yaxis.set_major_locator(MaxNLocator(integer=True))
 
-        # Set ticks to 10, 20, … 100
+        # Set X-axis ticks and labels
         plt.xticks(bins)
-
         plt.title("Age Distribution of Passengers")
         plt.xlabel("Age")
         plt.ylabel("Number of Passengers")
