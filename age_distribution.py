236bd83723beac959740f8d54b6f30f86dc98fcf
Erik A <196097247+ErikCoderMan@users.noreply.github.com>
2025-10-08 17:24:03 +0200

    Graph is now dynamic and includes ages above 100

--- age_distribution.py
+++ age_distribution.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
 
 class AgeDistribution:
     def __init__(self, passengers):
@@ -14,16 +15,20 @@ class AgeDistribution:
 
         # Create a histogram
         plt.figure(figsize=(8, 5))
-        plt.hist(ages, bins=range(0, 101, 10), edgecolor='black', alpha=0.7)
+        max_age = max(ages)
+        bins = range(0, (max_age // 10 + 2) * 10, 10)
+        plt.hist(ages, bins=bins, edgecolor='black', alpha=0.7)
+        ax = plt.gca()
+        ax.yaxis.set_major_locator(MaxNLocator(integer=True))
 
         # Set ticks to 10, 20, … 100
-        plt.xticks(range(10, 101, 10))
+        plt.xticks(bins)
 
         plt.title("Age Distribution of Passengers")
         plt.xlabel("Age")
         plt.ylabel("Number of Passengers")
         plt.grid(axis='y', alpha=0.3)
         plt.tight_layout()
-
+        
         # Show histogram
         plt.show()
