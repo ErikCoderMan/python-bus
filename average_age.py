fa2abbd417046bab02f3f63c5c198394c6e9304c
davidfjellstrom <56550285+davidfjellstrom@users.noreply.github.com>
2025-10-13 13:04:10 +0200

    Removed unnessecary line

--- average_age.py
+++ average_age.py
@@ -30,4 +30,3 @@ class AverageAge:
         # Skriver ut genomsnittsåldern med en decimal
         print(result)
         return(result) # För Pytest att läsa och jämföra resultat
-        print(f"Average age of passengers is: {average:.1f} years")
